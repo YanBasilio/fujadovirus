0542c93cfd9b7c26984b3881de6ee7966e44b343
Yan <yan.basilio@translogtransportes.com.br>
2020-04-23 17:57:59 -0300

    adsense

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-   
+    var tela = {'x': screen.width, 'y': screen.height};
+    
+    $('#parede_campo_top').attr('style','position:absolute; top:0px; left:0; width:'+tela.x+'px;height:1px;');
+    $('#parede_campo_bottom').attr('style','position:absolute; bottom:0px; left:0; width:'+tela.x+'px;height:1px;');
+    $('#parede_campo_right').attr('style','position:absolute; top:0px; right:0;width:1px;height:'+tela.y+'px;');
+    $('#parede_campo_left').attr('style','position:absolute; top:0px; left:0; width:1px;height:'+tela.y+'px;');
     insere_virus();
 });
 
@@ -18,30 +23,48 @@ function insere_virus(){
         perde_partida('virus');
     });
 
+    $('.parede_campo').on('mouseenter', function(){
+        perde_partida('saiu_tela');
+    });
+
     
     segue_ponteiro();
     setTimeout(() => {
         insere_virus();
-    }, 5000);
+    }, 2000);
+
+    var mouse_x = 0;
+    var mouse_y = 0;
+
+    $(document).on('mousemove', function(mouse){
+        mouse_x = mouse.clientX;
+        mouse_y = mouse.clientY;
+        $('.virus').animate({
+            top: mouse_y+'px',
+            left: mouse_x+'px'
+        }, "100");
+
+    });
+
+   
 }
 
 function segue_ponteiro(){
-    var position = getposition();
-    console.log(position);
+    
 }
 
 function getposition (){
-    $(document).on('mousemove', function(mouse){
-        var mouse_x = mouse.clientX;
-        var mouse_y = mouse.clientY;
-        
-        return {"mouse_x": mouse_x, "mouse_y":mouse_y}
-    });
+    
 }
 
 function perde_partida(tipo = 'virus'){
     var qtd = $('.virus').length;
-    $('#div-mensagem').find('#mensagem').html('Você perdeu com '+qtd+' virus.')
+    if(tipo == 'virus'){
+        $('#div-mensagem').find('#mensagem').html('Você perdeu com '+qtd+' virus.')
+    }else if(tipo == 'saiu_tela'){
+        $('#div-mensagem').find('#mensagem').html('Você perdeu. saiu do campo de jogo');
+
+    }
     $('#div-mensagem').show();
 
     setTimeout(() => {
